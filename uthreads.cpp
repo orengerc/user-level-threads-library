310020b353258c1912df766531d6ff1e47a714d7
oren_gerc <orengerc@gmail.com>
2022-03-28 13:24:01 +0300

    * moved classes into uthreads.cpp

--- uthreads.cpp
+++ uthreads.cpp
@@ -1,149 +1,95 @@
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "cert-err58-cpp"
+
 #include "uthreads.h"
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
 #include <setjmp.h>
 #include <signal.h>
-#include <unistd.h>
 #include <sys/time.h>
-#include <stdbool.h>
+#include <queue>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
 
 
+//-----------------------------------------------------classes----------------------------------------------------------
 
+#define DEFAULT_QUANTUM -1  //good default value?
 
-/**
- * @brief initializes the thread library.
- *
- * You may assume that this function is called before any other thread library function, and that it is called
- * exactly once.
- * The input to the function is the length of a quantum in micro-seconds.
- * It is an error to call this function with non-positive quantum_usecs.
- *
- * @return On success, return 0. On failure, return -1.
-*/
-int uthread_init(int quantum_usecs){
+/** Class that implements a single user-level-thread. */
+class UThread {
+private:
+    size_t tid_;
+    sigjmp_buf env_;  //what library to include for this guy?
+public:
+    UThread(size_t tid, void (*entry_point)()) {
+        /** Constructor:
+         * gets id, entrypoint
+         * sets up the environment with the addresses and everything
+         * saves with sigsetjmp
+         */
+    }
+
+    size_t get_tid() const { return tid_; };
+
+    void set_tid(size_t new_tid) { tid_ = new_tid; }
+};
+
+/** Class that manages UThreads using proper data-structures. */
+class UthreadsLib {
+private:
+    std::unordered_map<int, UThread *> all_threads_ = {};  //should allocate threads on the heap or stack???
+    std::unordered_set<int> blocked_tids_ = {};            //unordered because we want fast access
+    std::queue<int> ready_tids_ = {};
+    std::set<int> free_ids_ = {};                          //set saves the items in increasing order
+    int running_tid_ = {};
+    int quantum_ = {};
+public:
+    UthreadsLib() : UthreadsLib(DEFAULT_QUANTUM) {}
+
+    explicit UthreadsLib(int quantum_secs) : quantum_(quantum_secs) {
+        //init free_ids set
+    }
+
+    //getters and setters
+
+    //add new thread (called from spawn)
+
+    //block thread
+
+    //more functions we need
+};
+
+//----------------------------------------------------functions---------------------------------------------------------
+
+int uthread_init(int quantum_usecs) {
     //initialize new UthreadLib with quantum_secs
 }
 
-int get_lowest_free_tid(){
-    return 1;
-}
-
-/**
- * @brief Creates a new thread, whose entry point is the function entry_point with the signature
- * void entry_point(void).
- *
- * The thread is added to the end of the READY threads list.
- * The uthread_spawn function should fail if it would cause the number of concurrent threads to exceed the
- * limit (MAX_THREAD_NUM).
- * Each thread should be allocated with a stack of size STACK_SIZE bytes.
- *
- * @return On success, return the ID of the created thread. On failure, return -1.
-*/
-int uthread_spawn(thread_entry_point entry_point){
-
+int uthread_spawn(thread_entry_point entry_point) {
     //init thread with stack size 4096
-
-    new_thread->tid_ = get_lowest_free_tid();
-    all_threads.emplace(std::make_pair(new_thread->tid_, new_thread));
-
-    //add it to queue
+    //add it to ready queue
 }
 
-
-/**
- * @brief Terminates the thread with ID tid and deletes it from all relevant control structures.
- *
- * All the resources allocated by the library for this thread should be released. If no thread with ID tid exists it
- * is considered an error. Terminating the main thread (tid == 0) will result in the termination of the entire
- * process using exit(0) (after releasing the assigned library memory).
- *
- * @return The function returns 0 if the thread was successfully terminated and -1 otherwise. If a thread terminates
- * itself or the main thread is terminated, the function does not return.
-*/
-int uthread_terminate(int tid){
+int uthread_terminate(int tid) {
 
 }
 
-
-/**
- * @brief Blocks the thread with ID tid. The thread may be resumed later using uthread_resume.
- *
- * If no thread with ID tid exists it is considered as an error. In addition, it is an error to try blocking the
- * main thread (tid == 0). If a thread blocks itself, a scheduling decision should be made. Blocking a thread in
- * BLOCKED state has no effect and is not considered an error.
- *
- * @return On success, return 0. On failure, return -1.
-*/
-int uthread_block(int tid){
+int uthread_block(int tid) {
 
 }
 
+int uthread_resume(int tid) {}
 
-/**
- * @brief Resumes a blocked thread with ID tid and moves it to the READY state.
- *
- * Resuming a thread in a RUNNING or READY state has no effect and is not considered as an error. If no thread with
- * ID tid exists it is considered an error.
- *
- * @return On success, return 0. On failure, return -1.
-*/
-int uthread_resume(int tid){
+int uthread_sleep(int num_quantums) {}
 
-}
+int uthread_get_tid() {}
 
+int uthread_get_total_quantums() {}
 
-/**
- * @brief Blocks the RUNNING thread for num_quantums quantums.
- *
- * Immediately after the RUNNING thread transitions to the BLOCKED state a scheduling decision should be made.
- * After the sleeping time is over, the thread should go back to the end of the READY threads list.
- * The number of quantums refers to the number of times a new quantum starts, regardless of the reason. Specifically,
- * the quantum of the thread which has made the call to uthread_sleep isn’t counted.
- * It is considered an error if the main thread (tid==0) calls this function.
- *
- * @return On success, return 0. On failure, return -1.
-*/
-int uthread_sleep(int num_quantums){
-
-}
-
-
-/**
- * @brief Returns the thread ID of the calling thread.
- *
- * @return The ID of the calling thread.
-*/
-int uthread_get_tid(){
-
-}
-
-
-/**
- * @brief Returns the total number of quantums since the library was initialized, including the current quantum.
- *
- * Right after the call to uthread_init, the value should be 1.
- * Each time a new quantum starts, regardless of the reason, this number should be increased by 1.
- *
- * @return The total number of quantums.
-*/
-int uthread_get_total_quantums(){
-
-}
-
-
-/**
- * @brief Returns the number of quantums the thread with ID tid was in RUNNING state.
- *
- * On the first time a thread runs, the function should return 1. Every additional quantum that the thread starts should
- * increase this value by 1 (so if the thread with ID tid is in RUNNING state when this function is called, include
- * also the current quantum). If no thread with ID tid exists it is considered an error.
- *
- * @return On success, return the number of quantums of the thread with ID tid. On failure, return -1.
-*/
-int uthread_get_quantums(int tid){
-
-}
+int uthread_get_quantums(int tid) {}
 
 #pragma clang diagnostic pop
