f98d06157e6ce297da34f41c23a9eaf817c53c7a
oren_gerc <orengerc@gmail.com>
2022-03-29 20:12:50 +0300

    * added much stuff

--- uthreads.cpp
+++ uthreads.cpp
@@ -13,7 +13,11 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <set>
+#include <algorithm>
 
+/**********************************************************************************************************************
+ *****************************************   Constants & Defines  *****************************************************
+ *********************************************************************************************************************/
 
 #ifdef __x86_64__
 /* code for 64 bit Intel arch */
@@ -54,123 +58,144 @@ address_t translate_address(address_t addr)
     return ret;
 }
 
-
 #endif
 
-//-----------------------------------------------------classes----------------------------------------------------------
-
-#define DEFAULT_QUANTUM 0  //good default value??
 #define SUCCESS EXIT_SUCCESS
 #define FAILURE EXIT_FAILURE
-#define BLOCK_THREAD_ERROR (-1)
 
-/** Class that implements a single user-level-thread. */
+/**********************************************************************************************************************
+ ******************************************   Data Structures  ********************************************************
+ *********************************************************************************************************************/
+
+enum status
+{
+    MAIN,
+    READY,
+    RUNNING,
+    BLOCKED
+};
+
 class UThread
 {
-private:
-    int tid_;
-    sigjmp_buf env_;  //what library to include for this guy?
 public:
-    UThread(int tid, char *stack, thread_entry_point entry_point) : tid_(tid), env_{0}
+    int tid_;
+    status status_;
+    sigjmp_buf env_;
+    char stack_[STACK_SIZE] = {0};
+
+    UThread() : tid_(0), env_{0}, status_(MAIN)
     {
-        address_t sp = (address_t) stack + STACK_SIZE - sizeof(address_t);  //change for heap
+        sigsetjmp(env_, 1);
+        sigemptyset(&env_->__saved_mask);
+    }
+
+    UThread(int tid, thread_entry_point entry_point) : tid_(tid), env_{0}, status_(READY)
+    {
+        //save thread configuration
+        address_t sp = (address_t) stack_ + STACK_SIZE - sizeof(address_t);  //change for heap?
         address_t pc = (address_t) entry_point;
         sigsetjmp(env_, 1);
         (env_->__jmpbuf)[JB_SP] = translate_address(sp);
         (env_->__jmpbuf)[JB_PC] = translate_address(pc);
         sigemptyset(&env_->__saved_mask);  //needed?
     }
-
-    int get_tid() const
-    { return tid_; };
-
-    void set_tid(int new_tid)
-    { tid_ = new_tid; }
 };
 
-/** Class that manages UThreads using proper data-structures. */
-class UthreadsLib
+std::unordered_map<int, UThread *> all_threads_{};  //should allocate threads on the heap or stack???
+std::unordered_set<int> blocked_tids_{};            //unordered because we want fast access
+std::deque<int> ready_tids_{};
+std::set<int> free_ids_{};                          //set saves the items in increasing order
+int running_tid_{};
+int quantum_{};
+struct sigaction sa{};
+struct itimerval timer{};
+
+/**********************************************************************************************************************
+ **********************************************   Functions    ********************************************************
+ *********************************************************************************************************************/
+
+int initialize_main_thread()
 {
-private:
-    std::unordered_map<int, UThread> all_threads_ = {};  //should allocate threads on the heap or stack???
-    std::unordered_set<int> blocked_tids_ = {};            //unordered because we want fast access
-    std::deque<int> ready_tids_ = {};
-    std::set<int> free_ids_ = {};                          //set saves the items in increasing order
-    int running_tid_ = {};
-    int quantum_ = DEFAULT_QUANTUM;
-public:
-    UthreadsLib()
+    //allocate new UThread for main thread
+    auto new_uthread = new(std::nothrow) UThread;  // create new thread
+    if (!new_uthread)
     {
-        //init free_ids set
-        for (int i = 1; i <= MAX_THREAD_NUM; ++i) free_ids_.insert(i);
-
-        //init main thread - tid=0, directly into running
-
+        return FAILURE;
     }
 
-    //getters and setters
-    void set_quantum(int quantum)
-    {
-        quantum_ = quantum;
-    }
-
-    int get_quantum() const
-    {
-        return quantum_;
-    }
-
-    int get_running_tid() const
-    {
-        return running_tid_;
-    }
-
-    //add new thread (called from spawn)
-    int create_new_thread(char *stack_p, thread_entry_point entry_point)
-    {
-        if (free_ids_.empty())
-        {
-            return FAILURE;
-        }
-
-        UThread new_uthread = {*free_ids_.begin(), stack_p, entry_point};  // create new thread
-        free_ids_.erase(free_ids_.begin());  // erase the tid
-
-        //add new thread to all_threads_ and ready queue
-        all_threads_.emplace(new_uthread.get_tid(), new_uthread);
-        ready_tids_.push(new_uthread.get_tid());
-
-        return SUCCESS;
-    }
-
-    int block_thread(int tid)
-    {
-        // check if tid exists (as key in all_threads)
-
-        // check if tid is running - move running to blocked, move ready to running
-
-        // if it isn't running, move it to block
-    }
-
-    //more functions we need
-};
-
-//----------------------------------------------------functions---------------------------------------------------------
-
-UthreadsLib uthreads;  //maybe init from uthread_init is possible?
-
-int switch_threads()
-{
-    //high-level switch, return success if succeeded
-
-    //low level switch- sigset for current thread, siglongjump to new
+    //add it to the map
+    all_threads_[0] = new_uthread;
 
     return SUCCESS;
 }
 
-int init_sig_action()
+void cleanup()
 {
-    struct sigaction sa{};
-    sa.sa_handler = reinterpret_cast<_sig_func_ptr>(&switch_threads);  //good cast??
+    //deallocates all threads that were on the heap
+    for (auto &pair: all_threads_)
+    {
+        delete pair.second;
+    }
+}
+
+int start_timer()
+{
+    // Configure the timer to expire every quantum micro-seconds
+    timer.it_value.tv_sec = 0;
+    timer.it_value.tv_usec = quantum_;
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = quantum_;
+
+    // Start a virtual timer
+    if (setitimer(ITIMER_VIRTUAL, &timer, nullptr))
+    {
+        return FAILURE;
+    }
+
+    return SUCCESS;
+}
+
+int reset_timer()
+{
+    timer = {0};
+    start_timer();
+}
+
+int switch_ready_to_running()
+{
+    //save environment of current running thread, only if save succeeded, proceed to jump
+    if (sigsetjmp(all_threads_[running_tid_]->env_, 1))
+    {
+        /** deal with the error */
+        cleanup();
+        exit(EXIT_FAILURE);
+    }
+
+    //move first in ready to running
+    running_tid_ = ready_tids_.front();
+
+    //erase first in ready
+    ready_tids_.pop_front();
+
+    //reset timer
+    reset_timer();
+
+    //jump to new running tid
+    siglongjmp(all_threads_[running_tid_]->env_, 1);  //need to check return value??
+}
+
+int time_switch()
+{
+    //add running thread to ready queue
+    ready_tids_.emplace_back(running_tid_);
+
+    //switch first in ready to running
+    return switch_ready_to_running();
+}
+
+int initialize_sigaction()
+{
+    sa.sa_handler = reinterpret_cast<_sig_func_ptr>(&time_switch);  //good cast??
     if (sigaction(SIGVTALRM, &sa, nullptr) < 0)
     {
         /** deal with the error */
@@ -182,43 +207,35 @@ int init_sig_action()
     return SUCCESS;
 }
 
-int start_timer()
-{
-    struct itimerval timer{};
-
-    // Configure the timer to expire every quantum micro-seconds
-    timer.it_value.tv_sec = 0;
-    timer.it_value.tv_usec = uthreads.get_quantum();
-    timer.it_interval.tv_sec = 0;
-    timer.it_interval.tv_usec = uthreads.get_quantum();
-
-    // Start a virtual timer
-    if (setitimer(ITIMER_VIRTUAL, &timer, nullptr))
-    {
-        return FAILURE;
-    }
-
-    return SUCCESS;
-}
-
 int uthread_init(int quantum_usecs)
 {
+    //init main thread - tid=0, directly into running
+    if (SUCCESS != initialize_main_thread())
+    {
+        cleanup();
+        exit(EXIT_FAILURE);
+    }
+
+    //init free_ids set
+    for (int i = 1; i <= MAX_THREAD_NUM; ++i) free_ids_.insert(i);
+
+    /** check input */
     if (quantum_usecs <= 0)
     {
         /** deal with quantum_usecs error */
         return FAILURE;
     }
 
-    uthreads.set_quantum(quantum_usecs);
+    quantum_ = quantum_usecs;
 
     /** initialize sig-actions */
-    if (SUCCESS != init_sig_action())
+    if (SUCCESS != initialize_sigaction())
     {
         /** deal with setitimer error */
         return FAILURE;
     }
 
-    /** start timer */
+    /** initialize timer */
     if (SUCCESS != start_timer())
     {
         /** deal with setitimer error */
@@ -230,41 +247,111 @@ int uthread_init(int quantum_usecs)
 
 int uthread_spawn(thread_entry_point entry_point)
 {
-
-    void *stack_p = malloc(STACK_SIZE);  //allocate 4096 bytes of memory for stack of the new thread
-    if (!stack_p)
+    if (free_ids_.empty())
     {
         /** deal with the error */
         return FAILURE;
     }
 
-    int success = uthreads.create_new_thread((char *) stack_p, (thread_entry_point) entry_point);
-    if (SUCCESS != success)
+    auto new_uthread = new(std::nothrow) UThread{*free_ids_.begin(), entry_point};  // allocate new thread
+    if (!new_uthread)
     {
-        /** deal with the error */
-        return FAILURE;
+        cleanup();
+        exit(EXIT_FAILURE);
     }
+    free_ids_.erase(free_ids_.begin());  // erase the tid
+
+    //add new thread to all_threads_ and ready queue
+    all_threads_.emplace(new_uthread->tid_, new_uthread);
+    ready_tids_.emplace_back(new_uthread->tid_);
 
     return SUCCESS;
 }
 
 int uthread_terminate(int tid)
 {
+    //main thread termination is illegal
+    if (0 == tid)
+    {
+        cleanup();
+        exit(SUCCESS);
+    }
+
+    //if thread doesn't exist, return failure
+    if (all_threads_.find(tid) == all_threads_.end())
+    {
+        /** deal with the error */
+        return FAILURE;
+    }
+
+    //otherwise:
+    switch (all_threads_[tid]->status_)
+    {
+        case MAIN:
+            //irrelevant
+            break;
+        case RUNNING:
+            //what to do??
+            (void) (0);
+            break;
+        case READY:
+            ready_tids_.erase(std::find(ready_tids_.begin(), ready_tids_.end(), tid));
+            break;
+        case BLOCKED:
+            blocked_tids_.erase(tid);
+            break;
+    }
+
+    //return tid to free_tids
+    free_ids_.insert(tid);
+
+    //release memory of the terminated thread
+    delete all_threads_[tid];
+
+    //switch tid to nex_tid
+
+    //start timer again
+    reset_timer();
+
     return SUCCESS;
 }
 
 int uthread_block(int tid)
 {
-    // high level block
-    int new_running_tid = uthreads.block_thread(tid);
-    if (BLOCK_THREAD_ERROR == new_running_tid)
+    //try to block the main thread
+    if (0 == tid)
     {
-
+        /** deal with the error */
+        return FAILURE;
     }
 
-    if (new_running_tid != uthreads.get_running_tid())
+    //if thread doesn't exist, return failure
+    if (all_threads_.find(tid) == all_threads_.end())
     {
-        //switch to new thread
+        /** deal with the error */
+        return FAILURE;
+    }
+
+    //otherwise:
+    switch (all_threads_[tid]->status_)
+    {
+        case RUNNING:
+            blocked_tids_.insert(tid);
+            if (SUCCESS != switch_ready_to_running())
+            {
+                /** deal with the error */
+                return FAILURE;
+            }
+            break;
+        case READY:
+            ready_tids_.erase(std::find(ready_tids_.begin(), ready_tids_.end(), tid));
+            break;
+        case BLOCKED:
+            blocked_tids_.erase(tid);
+            break;
+        case MAIN:
+            //irrelevant
+            break;
     }
 
     return SUCCESS;
